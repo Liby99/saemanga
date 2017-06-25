aaf59b0c8d533dcc23ee01f269833f9a20cddd2c
Liby99 <liby99@icloud.com>
2017-06-25 12:01:54 -0700

    Modified Database Import Script

--- sql/manga.sql
+++ sql/manga.sql
@@ -4,6 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	5.7.10-enterprise-commercial-advanced
 
+CREATE DATABASE `Manga`;
+USE `Manga`;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
