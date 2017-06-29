cb5149a1ed7865480e46dfb1f3bcb5cead0d33ec
Liby99 <liby99@icloud.com>
2017-06-29 03:44:43 -0700

    Add User API and Test

--- source/api/user.js
+++ source/api/user.js
@@ -1,23 +1,175 @@
+const Crypto = require("keeling-js/lib/crypto");
 const Mongo = require("keeling-js/lib/mongo");
 const Users = Mongo.db.collection("user");
 
 module.exports = {
-    addUser(username, password) {
-        return Users.insert({
-            "username": username,
-            "password": password,
-            "register_date_time": new Date(),
-            "last_login": new Date(),
-            "visit_amount": new Date()
+    
+    /**
+     * Check if the user with the username already existed in the database.
+     * @param {string} username
+     * @param {Function} callback Callback that takes a boolean of whether the
+     * user existed
+     */
+    hasUser(username, callback) {
+        Users.findOne({
+            "username": username
+        }, function (err, user) {
+            if (err) {
+                throw new Error("Error checking user " + username + " existence: " + err);
+            }
+            else {
+                if (user) {
+                    callback(true);
+                }
+                else {
+                    callback(false);
+                }
+            }
         });
     },
-    removeUser(username) {
-        
+    
+    /**
+     * Add a new user to the database specified by username and password. Will
+     * abort if the username is already existed.
+     * @param {string} username
+     * @param {string} password
+     * @param {Function} callback Callback function takes a boolean indicating
+     * whether the add is successful
+     */
+    addUser(username, password, callback) {
+        Users.findOne({
+            "username": username
+        }, function (err, user) {
+            if (err) {
+                throw new Error("Error fetching user info: " + err);
+            }
+            else {
+                if (user) {
+                    callback(false);
+                }
+                else {
+                    var encrypted = Crypto.genEncrypted(password);
+                    Users.insert({
+                        "username": username,
+                        "password": encrypted,
+                        "register_date_time": new Date(),
+                        "last_login": new Date(),
+                        "visit_amount": 1,
+                        "following": [],
+                        "love": []
+                    }, function (err, userId) {
+                        if (err) {
+                            throw new Error("Error inserting new user " + username + ": " + err);
+                        }
+                        else {
+                            callback(true);
+                        }
+                    });
+                }
+            }
+        });
     },
-    changePassword(username, newPassword) {
-        
+    
+    /**
+     * Remove the user specified by the username
+     * @param {string} username
+     * @param {Function} callback Callback function takes a boolean of whether
+     * the remove is successful
+     */
+    removeUser(username, callback) {
+        Users.findOne({
+            "username": username
+        }, function (err, user) {
+            
+        });
     },
-    checkPassword(username, password) {
-        
+    
+    /**
+     * Change the user password. The user has to specify his old password
+     * correctly before setting up new password.
+     * @param {string} username
+     * @param {string} oldPassword
+     * @param {string} newPassword
+     * @param {Function} callback takes a boolean indicates whether the change
+     * password is success or not.
+     */
+    changePassword(username, oldPassword, newPassword, callback) {
+        Users.findOne({
+            "username": username
+        }, function (err, user) {
+            if (err) {
+                throw new Error("Error when fetching user " + username + " info: " + err);
+            }
+            else {
+                if (Crypto.match(oldPassword, user.password)) {
+                    var encNewPwd = Crypto.genEncrypted(newPassword);
+                    Users.update({
+                        "username": username
+                    }, {
+                        $set: {
+                            "password": encNewPwd
+                        }
+                    }, function (err, result) {
+                        if (err) {
+                            throw new Error("Error when updating user " + username + " password: " + err);
+                        }
+                        else {
+                            callback(true);
+                        }
+                    });
+                }
+                else {
+                    callback(false);
+                }
+            }
+        });
+    },
+    
+    /**
+     * Check the user login credential and update the user login infos. Will
+     * update the "last_login" to the current time and increment "visit_amount"
+     * by 1.
+     * @param {string} username
+     * @param {string} password
+     * @param {Function} callback Callback takes a boolean indicate whether
+     * the login is successful
+     */
+    login(username, password, callback) {
+        Users.findOne({
+            "username": username
+        }, function (err, user) {
+            if (err) {
+                throw new Error("Error when fetching user " + username + " info: " + err);
+            }
+            else {
+                if (user) {
+                    if (Crypto.match(password, user.password)) {
+                        Users.update({
+                            "username": username
+                        }, {
+                            $set: {
+                                "last_login": new Date()
+                            },
+                            $inc: {
+                                "visit_amount": 1
+                            }
+                        }, function (err, result) {
+                            if (err) {
+                                throw new Error("Error when updating user login info: " + err);
+                            }
+                            else {
+                                callback(true);
+                            }
+                        });
+                    }
+                    else {
+                        callback(false);
+                    }
+                }
+                else {
+                    callback(false);
+                }
+            }
+        });
     }
 }
---
+++ source/test/usertest.js
@@ -0,0 +1,60 @@
+const Mongo = require("keeling-js/lib/mongo");
+const config = require("../data/mongo.json");
+const assert = require("assert");
+
+const USERNAME_1 = "scripttest_1";
+const USERNAME_2 = "scripttest_2";
+const PASSWORD = "12345678";
+const WRONG_PASSWORD = "123456789";
+
+function testUser() {
+    console.log("Start Loading User Module... ");
+    const User = require("../api/user");
+    console.log("Passed");
+    
+    console.log("Testing Add User... ");
+    User.addUser(USERNAME_1, PASSWORD, function (success) {
+        assert.equal(success, true);
+        console.log("Passed");
+        
+        console.log("Testing Has User 1... ");
+        User.hasUser(USERNAME_1, function (has1) {
+            assert.equal(has1, true);
+            console.log("Passed");
+            
+            console.log("Testing Not Existing User 2... ");
+            User.hasUser(USERNAME_2, function (has2) {
+                assert.equal(has2, false);
+                console.log("Passed");
+                
+                console.log("Testing Successful Login... ");
+                User.login(USERNAME_1, PASSWORD, function (success) {
+                    assert.equal(success, true);
+                    console.log("Passed");
+                    
+                    console.log("Testing User Not Existed Login... ");
+                    User.login(USERNAME_2, PASSWORD, function (success) {
+                        assert.equal(success, false);
+                        console.log("Passed");
+                        
+                        console.log("Testing Wrong Password Login... ");
+                        User.login(USERNAME_1, WRONG_PASSWORD, function (success) {
+                            assert.equal(success, false);
+                            console.log("Passed");
+                        })
+                    });
+                });
+            });
+        });
+    });
+}
+
+function test() {
+    console.log("Initiating Mongo Module... ");
+    Mongo.init(config, function () {
+        console.log("Success!");
+        testUser();
+    });
+}
+
+test();
