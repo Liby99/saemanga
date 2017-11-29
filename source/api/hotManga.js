0b546837a430888f475a1e47cc967e97c6d72164
Liby Lee <liby99@icloud.com>
2017-11-28 16:24:08 -0800

    Done HotManga API

--- source/api/hotManga.js
+++ source/api/hotManga.js
@@ -1,13 +1,117 @@
+const ObjectID = require('mongodb').ObjectID;
+const Cartoonmad = require("./cartoonmad");
+const MangaType = require("./mangaType");
+const Debug = require("keeling-js/lib/debug");
 const Mongo = require("keeling-js/lib/mongo");
 const HotMangas = Mongo.db.collection("hot_manga");
 
 module.exports = {
     
-    clean () {
-        
+    /**
+     * Refresh the database of hot mangas. Basically clean up the database and
+     * then fetch all the data and insert them into database
+     * @return {[type]} [description]
+     */
+    refresh (callback) {
+        this.clear();
+        this.fetch(callback);
     },
     
-    addAll (ids) {
-        
+    /**
+     * Clear up the hot manga database
+     */
+    clear () {
+        HotMangas.remove({});
+    },
+    
+    /**
+     * Fetch the latest hot manga ids and latest mangas of each types
+     */
+    fetch (callback) {
+        var self = this;
+        self.fetchLatest(function () {
+            self.fetchAllTypes(callback);
+        });
+    },
+    
+    /**
+     * Fetch the latest manga
+     * @param  {Function} callback function when succeeded
+     */
+    fetchLatest (callback) {
+        Cartoonmad.getHotManga(function (ids) {
+            HotMangas.insertMany(ids.map((id) => {
+                return {
+                    "dmk_id": id,
+                    "type": "latest"
+                }
+            }), function (err) {
+                if (err) {
+                    Debug.error(err);
+                }
+                else {
+                    callback();
+                }
+            });
+        });
+    },
+    
+    fetchAllTypes (callback) {
+        var self = this;
+        MangaType.get(function (types) {
+            (function p(i) {
+                i < types.length ?
+                    self.fetchType(types[i], () => p(i + 1)) :
+                    callback()
+            })(0);
+        });
+    },
+    
+    fetchType (type, callback) {
+        Cartoonmad.getHotMangaOfType(type.dir, function (ids) {
+            HotMangas.insertMany(ids.map((id) => {
+                return {
+                    "dmk_id": id,
+                    "type": type["_id"]
+                };
+            }), function (err) {
+                if (err) {
+                    Debug.error(err);
+                }
+                else {
+                    callback();
+                }
+            });
+        });
+    },
+    
+    getLatestIds (callback) {
+        HotMangas.find({
+            "type": "latest"
+        }, {
+            "fields": {
+                "dmk_id": 1
+            }
+        }).toArray(function (err, ids) {
+            if (err) Debug.error(err);
+            else callback(ids.map((obj) => obj["dmk_id"]));
+        });
+    },
+    
+    getIdsOfType (typeId, callback) {
+        HotMangas.find({
+            "type": ObjectID(typeId)
+        }, {
+            "fields": {
+                "dmk_id": 1
+            }
+        }).toArray(function (err, ids) {
+            if (err) {
+                Debug.error(err);
+            }
+            else {
+                callback(ids.map((obj) => obj["dmk_id"]));
+            }
+        });
     }
 }
--- source/test/hotmanga.js
+++ source/test/hotmanga.js
@@ -3,37 +3,80 @@ const Mongo = require("keeling-js/lib/mongo");
 const config = require("../data/mongo.json");
 const Cartoonmad = require("../api/cartoonmad");
 
-function testGet(callback) {
-    console.log("-----Testing Getting Latest Manga-----");
-    Cartoonmad.getHotManga(function (ids) {
-        console.log("Latest: [" + ids + "]");
-        callback();
-    });
-}
-
-function testGetType(callback) {
-    console.log("-----Testing Getting Manga of Type-----");
-    const MangaType = require("../api/mangaType");
-    MangaType.get(function (types) {
-        (function p(i) {
-            if (i < types.length) {
-                Cartoonmad.getHotMangaOfType(types[i].dir, function (ids) {
-                    console.log(types[i].type + ": [" + ids + "]");
-                    p(i + 1);
-                });
-            }
-            else {
+var testList = [
+    
+    /**
+     * [testGet description]
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
+    function testGet (callback) {
+        console.log("-----Testing Getting Latest Manga-----");
+        Cartoonmad.getHotManga(function (ids) {
+            console.log("Latest: [" + ids + "]");
+            callback();
+        });
+    },
+    
+    /**
+     * [testGetType description]
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
+    function testGetType (callback) {
+        console.log("-----Testing Getting Manga of Type-----");
+        const MangaType = require("../api/mangaType");
+        MangaType.get(function (types) {
+            (function p(i) {
+                if (i < types.length) {
+                    Cartoonmad.getHotMangaOfType(types[i].dir, function (ids) {
+                        console.log(types[i].type + ": [" + ids + "]");
+                        p(i + 1);
+                    });
+                }
+                else {
+                    callback();
+                }
+            })(0);
+        });
+    },
+    
+    /**
+     * [testAPIFetch description]
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
+    function testAPIFetch (callback) {
+        console.log("-----Test Fetching-----");
+        const HotManga = require("../api/hotManga");
+        HotManga.refresh(function () {
+            console.log("Successfully Fetched All Hot Manga");
+            callback();
+        });
+    },
+    
+    /**
+     * [testGetFromDB description]
+     * @param  {Function} callback [description]
+     * @return {[type]}            [description]
+     */
+    function testGetFromDB (callback) {
+        console.log("-----Test Getting From DB-----");
+        const HotManga = require("../api/hotManga");
+        HotManga.getLatestIds(function (ids) {
+            console.log("Latest: [" + ids + "]");
+            HotManga.getIdsOfType("5a1df0ec880bd215441c6dd0", function (ids) {
+                console.log("Zhan Guo: [" + ids + "]");
                 callback();
-            }
-        })(0);
-    });
-}
+            });
+        });
+    }
+];
 
 function start() {
     console.log("Connecting to MongoDB...");
     Mongo.init(config, function () {
         console.log("Success!");
-        var testList = [ testGet, testGetType ];
         (function p(i) {
             i < testList.length ? testList[i](() => p(i + 1)) : Mongo.close()
         })(0);
