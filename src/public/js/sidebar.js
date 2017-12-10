d7f63e1a152333d75fa6ba7936a68057b448ff0e
Liby Lee <liby99@icloud.com>
2017-12-09 18:53:11 -0800

    Created Master Layout

--- src/public/js/sidebar.js
+++ src/public/js/sidebar.js
@@ -15,10 +15,20 @@ var Sidebar = {
         this.$toggle.children("i").addClass("active");
         this.$sidebar.addClass("active");
         this.$mask.fadeIn(200);
+        this.fixBody();
     },
     hide: function () {
         this.$toggle.children("i").removeClass("active");
         this.$sidebar.removeClass("active");
         this.$mask.fadeOut(200);
+        this.unfixBody();
+    },
+    fixBody: function () {
+        if (!$("body").hasClass("fixed"))
+            $("body").addClass("fixed").attr("data-top", $("body").scrollTop()).css({"margin-top": - $("body").scrollTop(), "position": "fixed"});
+    },
+    unfixBody: function () {
+        if ($("body").hasClass("fixed"))
+            $("body").removeClass("fixed").css({"margin-top": 0, "position": "relative"}).animate({"scrollTop": $("body").attr("data-top")}, 0);
     }
 }
