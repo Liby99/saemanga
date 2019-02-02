06c65c40c5862c517d07cf905ce82fc5154268dc
Liby Lee <liby99@icloud.com>
2019-02-02 01:39:35 -0800

    Updated minor script logs

--- src/script/refresh_all_manga.js
+++ src/script/refresh_all_manga.js
@@ -13,20 +13,19 @@ RunScript({
 
     // Then fetch all manga in ids
     Manga.count((amount) => {
-      const batch = amount / BATCH_SIZE;
-      (function process(i) {
-        if (i <= batch) {
+      (function process(numLeft) {
+        if (numLeft > 0) {
           Manga.updateOldest(BATCH_SIZE, () => {
-            console.log(`Done refreshing ${BATCH_SIZE} mangas`);
-            process(i + 1);
+            console.log(`- Batch completed: done refreshing ${BATCH_SIZE} mangas`);
+            process(numLeft - BATCH_SIZE);
           }, error);
         } else {
           const diff = new Date().getTime() - start.getTime();
-          console.log('Task finished');
+          console.log(`Task finished Done refreshing ${amount} mangas`);
           console.log(`Time elapsed: ${Math.round(diff / 1000)}s`);
           next();
         }
-      }(0));
+      }(amount));
     }, error);
   },
 });
